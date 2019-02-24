1b90f6bd1c8a9a2ba17c49eae9e2ae8ddc18f1db
dewiratnaps <dewiratnapramitasari123@gmail.com>
2019-02-24 08:08:59 +0700

    Video Eps.24

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -26,7 +26,16 @@ class KategoriController extends Controller
     		'kategori'=>'required|between:3,100|unique:kategori,nama_kategori',
     	])->validate();
 
-    	return 'Funngsi Save';
+    	$result = new Kategori;
+    	$result->nama_kategori = $req->kategori;
+
+    	if( $result->save() ){
+    		return redirect()->route('admin.kategori')
+    			->with('result','success');
+    	} else {
+    		return back()->with('result','fail')->withInput();
+    	}
+
     }
 
 }
